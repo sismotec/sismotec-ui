0bc1a57718b2b7fe7bddd4ea41eae5fea987bf5f
Alejandro Henkel <alehenkel17@gmail.com>
2017-09-27 06:13:18 -0500

    Update main component, add navbar layout and home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
 import createHistory from 'history/createBrowserHistory';
 import { Route } from 'react-router';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
-
 import createStore from './Data/Redux';
-import Home from './Containers/Home';
+
+// Import views
+import Layout from './Containers/Layout';
+import HomeView from './Containers/Home';
+import LoginView from './Containers/Login'
+
+// Build layout components for router
+const Home = () => <Layout slot={<HomeView />} />
+const Login = () => <Layout slot={<LoginView />} />
 
 // Create a browser history, and it's middleware
 const history = createHistory();
@@ -19,8 +25,14 @@ const App = _ => (
   <Provider store={store}>
     { /* ConnectedRouter will use the store from Provider automatically */ }
     <ConnectedRouter history={history}>
-      <Route exact path="/" component={Home} />
-      {/* <Route path="/about" component={About}/> */}
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        {/* <Route exact path="/registro" component={Register} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/dashboard/agregar" component={NeedCreate} />
+        <Route exact path="/dashboard/agregarOrden" component={OrderCreate} /> */}
+      </div>
     </ConnectedRouter>
   </Provider>
 );
