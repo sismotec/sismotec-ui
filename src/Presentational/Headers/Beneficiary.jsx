56cf83da8472e8373a43ab13a776259a86f92f09
Natalia <garcia.doe@gmail.com>
2017-09-27 16:28:47 -0500

    Added skeleton for headers

---
+++ src/Presentational/Headers/Beneficiary.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Beneficiary extends Component {
+  render() {
+    return (
+      <div>
+        Beneficiario
+      </div>
+    )
+  }
+}
---
+++ src/Presentational/Headers/CollectionCenter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CollectionCenter extends Component {
+  render() {
+    return (
+      <div>
+        Centro de acopio
+      </div>
+    )
+  }
+}
