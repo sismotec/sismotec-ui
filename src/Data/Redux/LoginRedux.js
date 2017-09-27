b5c7572ecd67780b4a8b684381e9654225e269da
Alejandro Henkel <alehenkel17@gmail.com>
2017-09-27 05:44:25 -0500

    Add login redux

--- src/Data/Redux/LoginRedux.js
+++ src/Data/Redux/LoginRedux.js
@@ -1,31 +1,47 @@
-import { createActions, createState, createReducer } from 'reduxsauce-polymer'
+import { createActions, createState, createReducer } from 'reduxsauce-crud';
 
 /* ------------- Types and Action Creators ------------- */
 
-export const LoginRedux = createActions({}, {
+export const LoginRedux = createActions({
+  loginRequest: ['data'],
+  loginSuccess: ['result'],
+  loginError: ['error'],
+  registerRequest: ['data'],
+  registerSuccess: ['result'],
+  registerError: ['error'],
+  logout: null,
+}, {
   prefix: 'LOGIN_',
-  defaultActions: {
-    get: true,
-  },
-})
+});
 
-const { Types, Creators } = LoginRedux
-export const LoginTypes = Types
-export default Creators
+const { Types, Creators } = LoginRedux;
+export const LoginTypes = Types;
+export default Creators;
 
 /* ------------- Initial State ------------- */
 
-export const INITIAL_STATE = createState({}, {
-  get: true,
-})
+export const INITIAL_STATE = createState({
+  fetching: false,
+  userId: null,
+  authToken: null,
+  error: null,
+});
 
 /* ------------- Reducers ------------- */
 
+const request = state => state.merge({ fetching: true });
+const success = (state, { userId }) => state.merge({ fetching: false, userId, error: null });
+const error = (state, { error }) => state.merge({ fetching: false, error });
+const logout = state => state.merge({ userId: null, });
+
 /* ------------- Hookup Reducers To Types ------------- */
 
-export const reducer = createReducer(INITIAL_STATE, {}, {
-  defaultActions: {
-    get: true,
-  },
-  Types,
-})
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.loginRequest]: request,
+  [Types.loginSuccess]: success,
+  [Types.loginError]: error,
+  [Types.registerRequest]: request,
+  [Types.registerSuccess]: success,
+  [Types.registerError]: error,
+  [Types.logout]: logout,
+});
