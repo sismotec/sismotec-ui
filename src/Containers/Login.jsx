b882a6945daa68c98d8cee9362cd3e9a1ce05551
LuisHernandez96 <luis_fernando_hernandez@outlook.com>
2017-09-27 16:31:46 -0500

    Login before material

--- src/Containers/Login.jsx
+++ src/Containers/Login.jsx
@@ -9,7 +9,7 @@ class Login extends Component {
     isAuth: PropTypes.bool,
     loginRequest: PropTypes.func,
     navigateToDashboard: PropTypes.func,
-  }
+  };
 
   componentWillMount() {
     // Redirect to dashboard
@@ -25,14 +25,98 @@ class Login extends Component {
       this.props.navigateToDashboard();
     }    
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        username: 'benis',
+        password: 'benis'
+      };
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleCreate = this.handleCreate.bind(this);
+    }
+
+    handleChange(event) {
+      const targetName = event.target.name;
+
+      let newstate = {
+            username: this.state.username,
+            password: this.state.password
+        };
+
+      newstate[targetName]=event.target.value;
+      this.setState(newstate);
+    }
+
+    handleSubmit(event) {
+        console.log(this.state);
+        event.preventDefault();
+        this.props.loginRequest({
+            username: this.state.username,
+            password: this.state.password
+        })
+    }
+
+    handleCreate(event) {
+        alert('Create new account');
+        event.preventDefault();
+    }
   
   render() {
     return (
-      <div>
-        Login
-      </div>
+        <div>
+            <h1>Entrar a mi cuenta</h1>
+            <form>
+                <div>
+                    <h2>Correo electrónico</h2>
+                    <input
+                        name='username'
+                        ref='username'
+                        type='text'
+                        label='Username'
+                        id='username'
+                        placeholder='semanai@itesm.mx'
+                        autoCorrect='off'
+                        autoCapitalize='off'
+                        spellCheck='false'
+                        defaultValue={this.state.username}
+                        onChange={this.handleChange}/>
+                </div>
+                <div>
+                    <h2>Contraseña</h2>
+                    <input
+                        name='password'
+                        ref='password'
+                        label='Password'
+                        id='password'
+                        type='password'
+                        placeholder='*********'
+                        defaultValue={this.state.password}
+                        onChange={this.handleChange}/>
+                </div>
+                <div>
+                    <br></br>
+                    <button
+                        type='submit'
+                        onSubmit={this.handleSubmit}>
+                    Entrar
+                    </button>
+                </div>
+                <div>
+                    <h2>¿Aún no tienes cuenta?</h2>
+                    <button
+                        type='submit'
+                        onSubmit={this.handleCreate}>
+                        Crear una cuenta
+                    </button>
+                </div>
+            </form>
+        </div>
     )
   }
+
 }
 
 const mapStateToProps = state => ({
--- src/Containers/Register.jsx
+++ src/Containers/Register.jsx
@@ -4,44 +4,75 @@ import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import LoginActions from '../Data/Redux/LoginRedux';
 
+class Field extends Component {
+    render() {
+        return(
+            <div>
+              <label>{this.props.fieldName}</label>
+              <br/>
+              <input type="text"/>
+            </div>
+        )
+    }
+}
+
 class Register extends Component {
-  static propTypes = {
-    isAuth: PropTypes.bool,
-    registerRequest: PropTypes.func,
-    navigateToDashboard: PropTypes.func,
-  }
+    static propTypes = {
+        isAuth: PropTypes.bool,
+        registerRequest: PropTypes.func,
+        navigateToDashboard: PropTypes.func,
+    }
 
-  componentWillMount() {
-    // Redirect to dashboard
-    if(this.props.isAuth) {
-      this.props.navigateToDashboard();
-    }    
-  }
-  
+    componentWillMount() {
+        // Redirect to dashboard
+        if(this.props.isAuth) {
+            this.props.navigateToDashboard();
+        }
+    }
 
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.isAuth) {
-      // Redirect to dashboard
-      this.props.navigateToDashboard();
-    }    
-  }
-  
-  render() {
-    return (
-      <div>
-        Register
-      </div>
-    )
-  }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.isAuth) {
+            // Redirect to dashboard
+            this.props.navigateToDashboard();
+        }
+    }
+
+    constructor() {
+        super();
+        this.state = {
+            name: null,
+            location: null,
+            phone: null,
+            email: null,
+            password: null,
+            passwordConfirmation: null,
+        };
+    }
+
+    render() {
+        return (
+            <div>
+              <h1>Registro de beneficiario</h1>
+              <Field fieldName="Nombre"/>
+              <Field fieldName="Ubicación"/>
+              <Field fieldName="Teléfono de contacto"/>
+              <Field fieldName="Correo electrónico"/>
+              <Field fieldName="Contraseña"/>
+              <Field fieldName="Confirmar contraseña"/>
+              <button>Registrarme</button>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({
-  isAuth: !!state.user.userId,
+    isAuth: !!state.user.userId,
 });
 
 const mapDispatchToProps = dispatch => ({
-  registerRequest: data => dispatch(LoginActions.registerRequest(data)),
-  navigateToDashboard: () => dispatch(push('/dashboard')),
+    registerRequest: data => dispatch(LoginActions.registerRequest(data)),
+    navigateToDashboard: () => dispatch(push('/dashboard')),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Register);
