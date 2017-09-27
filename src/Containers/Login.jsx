7064b1e85be8bd51d2b31d22e2b3b2396b149074
Alejandro Henkel <alehenkel17@gmail.com>
2017-09-27 06:13:29 -0500

    Login shell component

---
+++ src/Containers/Login.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import LoginActions from '../Data/Redux/LoginRedux';
+
+class Login extends Component {
+  static propTypes = {
+    isAuth: PropTypes.bool,
+    loginRequest: PropTypes.func,
+  }
+
+  componentWillMount() {
+    // Redirect to dashboard
+    if(this.props.isAuth) {
+      this.props.navigateToDashboard();
+    }    
+  }
+  
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+    if(nextProps.isAuth) {
+      // Redirect to dashboard
+      this.props.navigateToDashboard();
+    }    
+  }
+  
+  render() {
+    return (
+      <div>
+        Login
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  isAuth: !!state.user.userId,
+});
+
+const mapDispatchToProps = dispatch => ({
+  loginRequest: data => dispatch(LoginActions.loginRequest(data)),
+  navigateToDashboard: () => dispatch(push('/dashboard')),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
