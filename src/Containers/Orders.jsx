b39dc006e3e2e8eef614daa0c8de6899d23f0153
jorgegil96 <jorgegilcavazos@gmail.com>
2017-09-27 09:59:36 -0500

    Add table for orders view

--- src/Containers/Orders.jsx
+++ src/Containers/Orders.jsx
@@ -1,10 +1,104 @@
 import React, { Component } from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
+function ViewOrdersBtn() {
+    return <button>Mis ordenes</button>
+}
 
 export default class Orders extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          id: '823648', 
+          date: '19/07/17', 
+          poc: 'Juan Perez', 
+          status: 'Recibido', 
+          items: [{
+            name: "Agua", qty: "150", unit: "litros"
+          },
+          {
+            name: "Atún", qty: "300", unit: "latas"
+          }]
+        },
+        {
+          id: '213245', 
+          date: '22/07/17', 
+          poc: 'Raul Jimenez', 
+          status: 'No recibido',
+          items: [{
+            name: "Agua", qty: "150", unit: "L"
+          }]
+        },
+        {
+          id: '673244', 
+          date: '11/07/17', 
+          poc: 'Esteban Garza', 
+          status: 'Recibido',
+          items: [{
+            name: "Agua", qty: "150", unit: "L"
+          }]
+        }
+      ],
+
+    };
+  }
   render() {
+    const { data } = this.state;
     return (
       <div>
-        Orders
+        <ViewOrdersBtn />
+
+        <ReactTable
+          data={data}
+          columns={[
+            {
+              Header: "# Orden",
+              accessor: "id"
+            },
+            {
+              Header: "Fecha de creación",
+              accessor: "date"
+            },
+            {
+              Header: "Responsable",
+              accessor: "poc"
+            },
+            {
+              Header: "Status",
+              accessor: "status"
+            },
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+          SubComponent={row => {
+            return (
+              <div style={{ padding: "20px" }}>
+                <ReactTable
+                  data={data[0].items}
+                  columns={[
+                    {
+                      Header: "Nombre",
+                      accessor: "name"
+                    },
+                    {
+                      Header: "Cantidad",
+                      accessor: "qty"
+                    },
+                    {
+                      Header: "Unidad",
+                      accessor: "unit"
+                    }
+                  ]}
+                  defaultPageSize={5}
+                  showPagination={false}
+                />
+              </div>
+            );
+          }}
+        />
       </div>
     )
   }
