af91922c030056dfb2db5730e42ea1c8a2e1633c
Katie Arriaga M <katiearriaga@live.com>
2017-09-28 16:46:37 -0500

    Changed image url

--- src/Presentational/Headers/Title.jsx
+++ src/Presentational/Headers/Title.jsx
@@ -9,7 +9,7 @@ export default class Title extends Component {
       <div className="header-div">
         <img className="header-image"
           alt="logo"
-          src="https://scontent.fmfe1-1.fna.fbcdn.net/v/t35.0-12/s2048x2048/22119469_10155018692621814_1617782395_o.png?oh=51e8e7cce7457ca801ec27aec66c55ef&oe=59CF1103"
+          src="https://scontent.fmfe1-1.fna.fbcdn.net/v/t35.0-12/s2048x2048/22095643_10155018911241814_1980363964_o.png?oh=f0ee42ba173397c9451d18bf41872835&oe=59CFD51E"
         />
       </div>
     )
