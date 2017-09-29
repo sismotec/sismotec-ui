6dbaba739a69c06c5740ce2c8771cae773ec523a
Alejandro Henkel <alehenkel17@gmail.com>
2017-09-28 19:07:20 -0500

    Fix Register

--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ResourcesActions from '../Data/Redux/ResourcesRedux';
+import NeedsActions from '../Data/Redux/NeedsRedux';
 
 class Home extends Component {
 
   componentDidMount() {
-    this.props.getResources();  
+    this.props.getResources(); 
+    this.props.getNeeds();
   }
   
   render() {
@@ -20,6 +22,7 @@ const mapStateToProps = (state, ownProps) => ({})
 
 const mapDispatchToProps = dispatch => ({
   getResources: () => dispatch(ResourcesActions.getRequest()),
+  getNeeds: data => dispatch(NeedsActions.getOneRequest(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
