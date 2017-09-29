3c4c100aec16656390d4436918fdfe79c32e7606
Hector Rincon <hecerinc@hotmail.com>
2017-09-29 01:31:43 -0500

    Add dialog box. Close #23

--- src/Containers/MyNeeds.jsx
+++ src/Containers/MyNeeds.jsx
@@ -14,8 +14,11 @@ class MyNeeds extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.deleteNeed = this.deleteNeed.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleOkay = this.handleOkay.bind(this);
     this.data = {};
+    this.state = {open: false};
   }
   static propTypes = {
     needs: PropTypes.array,
@@ -28,9 +31,6 @@ class MyNeeds extends Component {
     needs: [],
   }
 
-  deleteNeed(index) {
-    //this.props.deleteNeed(this.data[index].id);
-  }
 
   componentWillMount() {
     // const { userId, getNeeds } = this.props
@@ -53,6 +53,9 @@ class MyNeeds extends Component {
         nombre: "cobijas",
         cantidad: 3,
         unidad: "cobijas",
+      },
+      {
+        type: "Delete",
       }
     ];
 
@@ -127,8 +130,21 @@ class MyNeeds extends Component {
   markComplete() {
     alert("Not implemented yet");
   }
+  handleOkay() {
+    // TODO: Implement this
+    alert("Handle deletion. Not yet implemented");
+    // Close it anyway
+    this.setState({open: false});
+  }
+  handleCancel() {
+    this.setState({open: false});
+  }
+  handleDeleteClick() {
+    this.setState({open: true});
+  }
   
   render() {
+    let ConfirmationDialog = this.ConfirmationDialog;
     return(
     <div className="container MyNeeds">
       <h1>Mis necesidades</h1>
@@ -144,7 +160,7 @@ class MyNeeds extends Component {
         <TableBody>
           {
             this.fields.map((n, index) => 
-              <CustomRow need={n} key={index} id={index} handleChange={this.handleChange} deleteAction={this.deleteNeed} markComplete={this.markComplete}/>
+              <CustomRow need={n} key={index} id={index} handleChange={this.handleChange} deleteAction={this.handleDeleteClick} markComplete={this.markComplete}/>
             )
           }
         </TableBody>
@@ -155,25 +171,27 @@ class MyNeeds extends Component {
         color="primary"
         onClick={() => this.props.updateNeeds(this.props.userId, this.data)}>Guardar cambios
       </Button>
+      <ConfirmationDialog open={this.state.open} orc={this.handleCancel} />
     </div>)
   }
-}
+  ConfirmationDialog = (props) => {
+    let open = props.open;
+    return (
+      <Dialog
+        maxWidth="xs"
+        open={open}
+        onRequestClose={props.orc}
+      >
+        <DialogTitle>Confirmaci&oacute;n</DialogTitle>
+        <DialogContent>&iquest;Est&aacute;s seguro de que quieres borrar este recurso? </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleOkay} color="primary">S&iacute;</Button>
+          <Button onClick={this.handleCancel} color="error">No</Button>
+        </DialogActions>
+      </Dialog>
+    )
+  }
 
-const confirmationDialog() {
-  return (
-    <Dialog
-      ignoreBackdropClick
-      ignoreEscapeKeyUp
-      maxWidth="xs"
-    >
-      <DialogTitle>Confirmaci&oacute;n</DialogTitle>
-      <DialogContent> Est&aacute;s seguro de que quieres borrar este recurso? </DialogContent>
-      <DialogActions>
-        <Button color="primary">S&iacute;</Button>
-        <Button color="error">No</Button>
-      </DialogActions>
-    </Dialog>
-  )
 }
 const mapStateToProps = state => ({
   userId: state.user.userId,
